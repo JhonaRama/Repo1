619fda923d1ce2cecb83063ed8451820a1207ddc
Usuario <Usuario@DESKTOP-7GHL1FQ>
2021-11-11 17:59:51 -0500

    Inicio
    -clase1: software
    -clase2: tipos de datos

---
+++ pruebas/src/main/java/pruebas/TiposDeDatos.java
@@ -0,0 +1,49 @@
+package pruebas;
+
+public class TiposDeDatos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int edad = 10;
+		
+		// comentario
+		
+				// tipos de datos
+				// edad > numerico
+				// byte < short < int < long < float < double
+				
+				// tipo nombre = 10;
+				byte edad1 = 10;
+				short edadShort = 10;
+				int edadInt = 10;
+				long edadLong = 10;
+				float edadFloat = 10;
+				double edadDouble = edadFloat;
+				
+				// edadInt = edadFloat; no se puede hacer
+				
+				//definir
+				String nombre;
+				
+				//asignar
+				nombre = "carlos";
+				
+				boolean flag = true;
+				boolean otroFlag = false;
+
+				char letra = 'a';
+				
+				
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
